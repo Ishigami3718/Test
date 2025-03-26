23218761c4828f8453a2b21d667c95b8cce48fab
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2025-03-26 16:23:56 +0200

    init

---
+++ prod/Program.cs
@@ -0,0 +1,43 @@
+﻿using prod;
+using System;
+namespace Lab
+{
+    class Program
+    {
+        static int[,] CreateArray()
+        {
+            ///code
+            int[,] arr = new int[1, 1];
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            string choose = "1";
+            int[,] arr;
+            while (choose != "0")
+            {
+                choose = Console.ReadLine();
+                Console.WriteLine("Input create array style");
+                switch (choose)
+                {
+                    case "1":arr = CreateArray(); break;
+                    case "2": arr = CreateArray(); break;
+                    case "3": arr = CreateArray(); break;
+                }
+                string chooseSrudent = "1";
+                while (chooseSrudent != "0")
+                {
+                    choose = Console.ReadLine();
+                    Console.WriteLine("Input block to do");
+                    switch (choose)
+                    {
+                        case "1": arr = Student1.Block1(); break;
+                        case "2": arr = CreateArray(); break;
+                        case "3": arr = CreateArray(); break;
+                    }
+
+                }
+            }
+        }
+    }
+}
